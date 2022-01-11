e611a196346c502f65600369803f9515426c7bff
Lucas <lucas.ribeiro711@gmail.com>
2022-01-10 22:58:04 -0300

    :lipstick: style: edit theme colors

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      light: '#FFFFFF',
+      dark: '#111010',
+      primary: '#0284c7',
+      secondary: '#0369a1'
+    },
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
