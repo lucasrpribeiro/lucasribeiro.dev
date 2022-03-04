d5ffeaa41828c7d5bfe1df14b0d7f9de8973a84e
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2022-03-04 00:13:12 -0300

    :lipstick: style: restyle

---
+++ components/The/BoxMe/BoxMe.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="$style.boxMe">
+    <img src="https://avatars.githubusercontent.com/u/49355209?v=4" alt="Uma foto minha" :class="$style.profImg">
+    <div :class="$style.sec">
+      <strong>Lucas Ribeiro</strong>
+      <span>Olá, eu sou Lucas, desenvolvo para a web desde 2021 utilizando JavaScript, VueJS, NuxtJS e TailwindCSS</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheBoxMe'
+}
+</script>
+
+<style lang="postcss" module>
+.boxMe {
+  @apply flex flex-col gap-2 justify-center items-center p-4 rounded-md w-1/2 bg-full-blue-100;
+}
+
+.sec {
+  @apply flex flex-col text-center;
+}
+
+.profImg {
+  @apply w-60 rounded-full m-auto;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <TheNavbar />
+  <section :class="$style.mySection">
+    <TheBoxMe />
+  </section>
 </template>
 
 <script>
@@ -7,3 +9,9 @@ export default {
   name: 'IndexPage'
 }
 </script>
+
+<style lang="postcss" module>
+  .mySection {
+    @apply min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-blue-sky-500 to-full-blue-500;
+  }
+</style>
