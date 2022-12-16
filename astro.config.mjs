261cdd3f2a7c0156e1800d71ac1f56ebdb2040c8
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2022-12-16 11:36:53 -0300

    chore(deps): install astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,9 @@ import sitemap from "@astrojs/sitemap";
 import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue(), sitemap(), robotsTxt()]
+  integrations: [tailwind(), vue(), sitemap(), robotsTxt(), image()]
 });
