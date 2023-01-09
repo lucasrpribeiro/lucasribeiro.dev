cb468e7fe9061b999570e6f76d7edd294b631422
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-01-09 18:40:28 -0300

    fix: colors

--- src/components/vue/MyServices.vue
+++ src/components/vue/MyServices.vue
@@ -2,26 +2,26 @@
 const services = [
   {
     name: 'Otimização de Busca (SEO)',
-    role: 'Seja prioridade na busca do Google!',
+    role: 'Otimização para motores de busca para deixar seu site na primeira página do Google convertendo muito mais resultados.',
     imageUrl:
       '/images/icones/lupa.png',
   },
   {
     name: 'Velocidade',
-    role: 'Não perca clientes por conta da velocidade do seu site!',
+    role: 'Não perca clientes por conta da demora em carregar o seu site!',
     imageUrl:
       '/images/icones/cronometro.png',
   },  {
-    name: 'Engajamento',
-    role: 'Sua marca precisa estar onde as pessoas estão, na internet!',
+    name: 'Design',
+    role: 'Utilize o design como diferencial competitivo, gerando experiências inovadoras e criativas a seus clientes e consumidores.',
     imageUrl:
-      '/images/icones/click.png',
+      '/images/icones/apresentacao.png',
   },
   {
     name: 'Apresentação',
-    role: 'Não basta ser encontrado, você precisa causar uma boa impreção!',
+    role: 'Entregue uma boa impressão aí seu cliente, com sites modernos e adaptados as mais diversas telas.',
     imageUrl:
-      '/images/icones/apresentacao.png',
+      '/images/icones/click.png',
   },
   // More services...
 ]
@@ -32,16 +32,16 @@ const services = [
     <div class="container mx-auto py-12 px-4 text-center sm:px-6">
       <div class="space-y-8 sm:space-y-12">
         <div class="space-y-5 sm:mx-auto sm:space-y-4">
-          <h2 class="text-3xl font-bold font-orbitron text-transparent bg-clip-text bg-gradient-to-r from-lksYellow to-lksGreen tracking-tight sm:text-4xl">Serviços</h2>
+          <h2 class="justNeon text-3xl font-bold font-orbitron text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-cyan-500 tracking-tight sm:text-4xl">Serviços</h2>
           <p class="text-xl">Como eu posso te ajudar a ser encontrado.</p>
         </div>
         <ul role="list" class="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:gap-x-12 lg:gap-y-12">
           <li v-for="service in services" :key="service.name">
-            <div class="space-y-4 rounded-xl bg-gray-900 shadow-xl bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-50 p-4 h-52 md:h-60 flex flex-col items-center justify-center gap-2">
-              <img class="mx-auto h-20 w-20 bg-gradient-to-tr from-lksYellow to-lksGreen rounded-full lg:h-24 lg:w-24 shadow-md shadow-lksYellow" :src="service.imageUrl" alt="" />
+            <div class="rounded-xl bg-black shadow-md cursor-pointer bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-50 p-4 min-h-[296px] flex flex-col items-center  gap-4 hover:shadow-lg hover:shadow-teal-500 lg:hover:-translate-y-4 hover:duration-300">
+              <img class="mx-auto h-20 w-20 bg-gradient-to-tr from-cyan-700 to-teal-400 rounded-full lg:h-24 lg:w-24 shadow-md shadow-teal-500" :src="service.imageUrl" alt="" />
               <div class="space-y-2">
                 <div class="text-xs font-medium lg:text-sm space-y-2">
-                  <h3>{{ service.name }}</h3>
+                  <h3 class="font-orbitron font-bold">{{ service.name }}</h3>
                   <p class="font-light ">{{ service.role }}</p>
                 </div>
               </div>
@@ -52,3 +52,9 @@ const services = [
     </div>
   </div>
 </template>
+
+<style>
+.justNeon {
+  text-shadow: #12e7f6 1px 0 10px;
+}
+</style>
