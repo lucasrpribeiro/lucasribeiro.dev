ec4649b3cfed082a9dcd2d0a55348e3fcca3ce4a
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-02-18 17:13:15 -0300

    fix: mobile menu

--- src/components/vue/HeroSection.vue
+++ src/components/vue/HeroSection.vue
@@ -8,29 +8,55 @@ const navigation = [
   { name: 'Portifolio', href: '/#portifolio' },
   { name: 'Caontato', href: '/#contact' },
 ]
-
-const count = () => {
-  for(let n = 0; n <= 548; n++) {
-    return n + 1
-  }
-}
 </script>
 
 <template>
   <div>
     <div class="relative overflow-hidden">
-      <div class="relative pt-6 pb-16 sm:pb-24">
+      <div class="relative pb-16 sm:pb-24">
         <Popover>
-          <div class="mx-auto container px-4 sm:px-6">
-            <nav class="relative flex items-center justify-between sm:h-10 md:justify-center" aria-label="Global">
-              <div class="flex flex-1 items-center md:absolute md:inset-y-0 md:left-0">
+          <div class="mx-auto container p-4">
+            <nav
+              class="
+                relative
+                flex
+                items-center
+                justify-between
+                sm:h-10
+                md:justify-center
+              "
+              aria-label="Global"
+            >
+              <div
+                class="
+                  flex flex-1
+                  items-center
+                  md:absolute md:inset-y-0 md:left-0
+                "
+              >
                 <div class="flex w-full items-center justify-between md:w-auto">
                   <a href="#">
                     <span class="sr-only">Lucas Ribeiro - Web Developer</span>
-                    <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                    <img
+                      class="h-8 w-auto sm:h-10"
+                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                      alt=""
+                    />
                   </a>
-                  <div class="-mr-2 flex items-center md:hidden">
-                    <PopoverButton class="inline-flex items-center justify-center rounded-md bg-gradient-to-br from-teal-500 to-cyan-500 p-2 text-zinc-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-teal-500 duration-300">
+                  <div class="flex items-center md:hidden">
+                    <PopoverButton
+                      class="
+                        inline-flex
+                        items-center
+                        justify-center
+                        rounded-md
+                        bg-white bg-opacity-5
+                        p-2
+                        text-white
+                        focus:outline-none focus:ring-0 focus:ring-customBlue
+                        duration-300
+                      "
+                    >
                       <span class="sr-only">Open main menu</span>
                       <Bars3Icon class="h-6 w-6" aria-hidden="true" />
                     </PopoverButton>
@@ -38,72 +64,295 @@ const count = () => {
                 </div>
               </div>
               <div class="hidden md:flex md:space-x-10">
-                <a v-for="item in navigation" :key="item.name" :href="item.href" class="font-medium hover:text-teal-500 duration-300">{{ item.name }}</a>
+                <a
+                  v-for="item in navigation"
+                  :key="item.name"
+                  :href="item.href"
+                  class="font-medium hover:text-teal-500 duration-300"
+                  >{{ item.name }}</a
+                >
               </div>
-              <div class="hidden md:absolute md:inset-y-0 md:right-0 md:flex md:items-center md:justify-end">
+              <div
+                class="
+                  hidden
+                  md:absolute
+                  md:inset-y-0
+                  md:right-0
+                  md:flex
+                  md:items-center
+                  md:justify-end
+                "
+              >
                 <span class="inline-flex rounded-md shadow">
-                  <a href="#" class="inline-flex items-center rounded-md border border-transparent bg-gradient-to-r from-teal-500 to-cyan-500 px-4 py-2 text-base font-medium hover:shadow-lg hover:scale-110 duration-300 text-gray-50 ">Entre em contato!</a>
+                  <a
+                    href="#"
+                    class="
+                      inline-flex
+                      items-center
+                      rounded-md
+                      border border-transparent
+                      bg-gradient-to-r
+                      from-teal-500
+                      to-cyan-500
+                      px-4
+                      py-2
+                      text-base
+                      font-medium
+                      hover:shadow-lg hover:scale-110
+                      duration-300
+                      text-gray-50
+                    "
+                    >Entre em contato!</a
+                  >
                 </span>
               </div>
             </nav>
           </div>
           <!-- Mobile Menu Panel -->
-          <transition enter-active-class="duration-150 ease-out" enter-from-class="opacity-0 scale-95" enter-to-class="opacity-100 scale-100" leave-active-class="duration-100 ease-in" leave-from-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-95">
-            <PopoverPanel focus class="absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden">
-              <div class="overflow-hidden rounded-lg bg-haiti-900 shadow-xl bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-50">
-                <div class="flex items-center justify-between px-5 pt-4">
+          <transition
+            enter-active-class="duration-150 ease-out"
+            enter-from-class="opacity-0 scale-95"
+            enter-to-class="opacity-100 scale-100"
+            leave-active-class="duration-100 ease-in"
+            leave-from-class="opacity-100 scale-100"
+            leave-to-class="opacity-0 scale-95"
+          >
+            <PopoverPanel
+              focus
+              class="
+                absolute
+                inset-x-0
+                top-0
+                z-10
+                origin-top-right
+                transform
+                transition
+                md:hidden
+              "
+            >
+              <div
+                class="
+                  overflow-hidden
+                  rounded-lg
+                  bg-haiti-900
+                  shadow-xl
+                  bg-clip-padding
+                  backdrop-filter backdrop-blur-md
+                  bg-opacity-50
+                "
+              >
+                <div class="flex items-center justify-between p-4">
                   <div>
-                    <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                    <img
+                      class="h-8 w-auto"
+                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                      alt=""
+                    />
                   </div>
-                  <div class="-mr-2">
-                    <PopoverButton class="inline-flex items-center justify-center rounded-md bg-gradient-to-br from-teal-500 to-cyan-500 p-2 text-zinc-100 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-500">
+                  <div>
+                    <PopoverButton
+                      class="
+                        inline-flex
+                        items-center
+                        justify-center
+                        rounded-md
+                        bg-white bg-opacity-5
+                        p-2
+                        text-white
+                        focus:outline-none focus:ring-0 focus:ring-customBlue
+                        duration-300
+                      "
+                    >
                       <span class="sr-only">Close main menu</span>
                       <XMarkIcon class="h-6 w-6" aria-hidden="true" />
                     </PopoverButton>
                   </div>
                 </div>
                 <div class="space-y-1 px-2 pt-2 pb-3">
-                  <a v-for="item in navigation" :key="item.name" :href="item.href" class="block rounded-md px-3 py-2 text-base font-medium  hover:bg-teal-500 duration-300">{{ item.name }}</a>
+                  <a
+                    v-for="item in navigation"
+                    :key="item.name"
+                    :href="item.href"
+                    class="
+                      block
+                      rounded-md
+                      px-3
+                      py-2
+                      text-base
+                      font-medium
+                      duration-300
+                    "
+                    >{{ item.name }}</a
+                  >
+                  <a
+                  href="#"
+                  class="
+                    block
+                    bg-customBlue
+                    px-5
+                    py-3
+                    text-center
+                    font-medium
+                    rounded-lg
+                  "
+                  >Entre em contato!</a
+                >
                 </div>
-                <a href="#" class="block w-full bg-gradient-to-r from-teal-500 to-cyan-500 px-5 py-3 text-center font-medium text-zinc-50">Entre em contato!</a>
               </div>
             </PopoverPanel>
           </transition>
         </Popover>
         <!-- Hero -->
-        <div class="container flex flex-col items-center justify-center gap-6 lg:flex-row mx-auto pt-5 px-4">
+        <div
+          class="
+            container
+            flex flex-col
+            items-center
+            justify-center
+            gap-6
+            lg:flex-row
+            mx-auto
+            pt-5
+            px-4
+          "
+        >
           <div class="text-center lg:text-left flex-1">
-            <h1 class="font-orbitron text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl">
-              <span class="block">Olá, eu sou <span class="text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-cyan-500">Lucas!</span></span>
+            <h1
+              class="
+                font-orbitron
+                text-4xl
+                font-bold
+                tracking-tight
+                sm:text-5xl
+                md:text-6xl
+              "
+            >
+              <span class="block"
+                >Olá, eu sou
+                <span
+                  class="
+                    text-transparent
+                    bg-clip-text bg-gradient-to-r
+                    from-teal-500
+                    to-cyan-500
+                  "
+                  >Lucas!</span
+                ></span
+              >
               <span class="block">Programador Web</span>
             </h1>
-            <p class="mt-3 max-w-md text-sm sm:text-base md:mt-10 md:max-w-3xl md:text-3xl">Como desenvolvedor web especializado em <span class="text-cyan-500 font-bold">SEO</span> e <span class="text-cyan-500 font-bold">performance</span>, posso ajudar seu negócio a alcançar novos patamares de sucesso online.</p>
-            <p class="mt-3 max-w-md text-sm sm:text-base md:mt-5 md:max-w-3xl md:text-3xl">
-              Com minha abordagem de otimização de mecanismos de busca, posso <span class="text-cyan-500 font-bold">melhorar o seu ranking</span> nos resultados de busca, aumentando seu tráfego e alcançando <span class="text-cyan-500 font-bold">novos clientes</span>. 
+            <p
+              class="
+                mt-3
+                max-w-md
+                text-sm
+                sm:text-base
+                md:mt-10 md:max-w-3xl md:text-3xl
+              "
+            >
+              Como desenvolvedor web especializado em
+              <span class="text-cyan-500 font-bold">SEO</span> e
+              <span class="text-cyan-500 font-bold">performance</span>, posso
+              ajudar seu negócio a alcançar novos patamares de sucesso online.
+            </p>
+            <p
+              class="
+                mt-3
+                max-w-md
+                text-sm
+                sm:text-base
+                md:mt-5 md:max-w-3xl md:text-3xl
+              "
+            >
+              Com minha abordagem de otimização de mecanismos de busca, posso
+              <span class="text-cyan-500 font-bold"
+                >melhorar o seu ranking</span
+              >
+              nos resultados de busca, aumentando seu tráfego e alcançando
+              <span class="text-cyan-500 font-bold">novos clientes</span>.
             </p>
           </div>
-          <section class="flex flex-col items-center justify-center py-6 flex-1 lg:max-w-[340px] xl:max-w-[500px]">
-          <section>
-            <picture class="relative">
-              <!-- <source srcset="" media="(min-width: 900px)" />
+          <section
+            class="
+              flex flex-col
+              items-center
+              justify-center
+              py-6
+              flex-1
+              lg:max-w-[340px]
+              xl:max-w-[500px]
+            "
+          >
+            <section>
+              <picture class="relative">
+                <!-- <source srcset="" media="(min-width: 900px)" />
               <source srcset="" media="(min-width: 600px)" /> -->
-              <img draggable="false" src="/images/lucas.png" alt="Lucas Ribeiro" width="1024" class="relative z-10" />
-              <div class="absolute top-0 right-2 w-[500px] h-[500px] rounded-full bg-gradient-to-t from-teal-500 to-cyan-500 blur-[100px] animate-pulse-slow hidden lg:block"></div>
-            </picture>
-            
+                <img
+                  draggable="false"
+                  src="/images/lucas.png"
+                  alt="Lucas Ribeiro"
+                  width="1024"
+                  class="relative z-10"
+                />
+                <div
+                  class="
+                    absolute
+                    top-0
+                    right-2
+                    w-[500px]
+                    h-[500px]
+                    rounded-full
+                    bg-gradient-to-t
+                    from-teal-500
+                    to-cyan-500
+                    blur-[100px]
+                    animate-pulse-slow
+                    hidden
+                    lg:block
+                  "
+                ></div>
+              </picture>
+            </section>
+            <section
+              class="
+                relative
+                -mt-11
+                pt-9
+                pb-4
+                w-full
+                bg-haiti-900
+                rounded-xl
+                bg-clip-padding
+                backdrop-filter backdrop-blur-md
+                bg-opacity-30
+                text-6xl
+                flex flex-col
+                items-center
+                justify-center
+                font-orbitron
+                gap-4
+                z-20
+              "
+            >
+              <div>
+                <span>578</span>
+              </div>
+              <span
+                class="flex gap-1 justify-center items-center text-base italic"
+                ><span
+                  ><svg width="32" height="32" viewBox="0 0 24 24" class="w-12">
+                    <path
+                      fill="currentColor"
+                      class="text-cyan-500"
+                      d="M20 3H4v10c0 2.21 1.79 4 4 4h6c2.21 0 4-1.79 4-4v-3h2a2 2 0 0 0 2-2V5c0-1.11-.89-2-2-2zm0 5h-2V5h2v3zM4 19h16v2H4z"
+                    />
+                  </svg>
+                </span>
+                Canecas de café!</span
+              >
+              <!-- <img src="/images/3d-clock.png" class="absolute -bottom-12 w-24"/> -->
+            </section>
           </section>
-          <section class="relative -mt-11 pt-9 pb-4 w-full bg-haiti-900 rounded-xl bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 text-6xl flex flex-col items-center justify-center font-orbitron gap-4 z-20">
-            <div>
-              <span>578</span>
-            </div>
-            <span class="flex gap-1 justify-center items-center text-base italic"><span><svg width="32" height="32" viewBox="0 0 24 24" class="w-12">
-                  <path fill="currentColor" class="text-cyan-500"
-                    d="M20 3H4v10c0 2.21 1.79 4 4 4h6c2.21 0 4-1.79 4-4v-3h2a2 2 0 0 0 2-2V5c0-1.11-.89-2-2-2zm0 5h-2V5h2v3zM4 19h16v2H4z" />
-                </svg>
-              </span> Canecas de café!</span>
-            <!-- <img src="/images/3d-clock.png" class="absolute -bottom-12 w-24"/> -->
-          </section>
-        </section>
         </div>
       </div>
     </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,48 +8,13 @@ module.exports = {
 				'pulse-slow': 'pulse 5s linear infinite',
       },
 			fontFamily: {
-				firaCode: ["Fira Code", 'monospace'],
-				roboto: ["Roboto", 'sans-serif'],
-				tech : ["Share Tech Mono", 'monospace'],
-				orbitron: ["Orbitron", 'sans-serif']
+				inter: ['Inter', 'sans-serif'],
 			},
 			colors: {
-				seance: {
-					DEFAULT: '#451B66',
-					'50': '#F8E4F6',
-					'100': '#F2CCF0',
-					'200': '#E29CE5',
-					'300': '#CB6CD8',
-					'400': '#AE3BCB',
-					'500': '#7E2A9F',
-					'600': '#602383',
-					'700': '#451B66',
-					'800': '#2D144A',
-					'900': '#190C2E'
-				},
-				haiti: {
-					DEFAULT: '#320f60',
-					'50': '#f3f0fd',
-					'100': '#d9cbfc',
-					'200': '#b497f8',
-					'300': '#8c5bed',
-					'400': '#6a29d8',
-					'500': '#5210bc',
-					'600': '#440a97',
-					'700': '#3a0d78',
-					'800': '#320f60',
-					'900': '#050209',
-			},
-				darkBlue: '#2A2634',
-				lksGreen: '#2ecc71',
-				lksYellow: '#f1c40f',
-			},
-			textShadow: {
-        sm: '0 1px 2px var(--tw-shadow-color)',
-        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
-        lg: '0 8px 16px var(--tw-shadow-color)',
-      },
-		},
+				customBlue: '#1C56B8',
+				primary: '#0B0B0D'
+			}
+		}
 	},
 	plugins: [
 		require('@tailwindcss/aspect-ratio')
