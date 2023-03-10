d368a8371a8d3690e6f1ddb6cd8274a119ab9da7
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-03-10 11:19:32 -0300

    chore: install partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,14 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
+import partytown from '@astrojs/partytown';
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import vue from "@astrojs/vue";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 import robotsTxt from 'astro-robots-txt';
-
-// https://astro.build/config
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue(), sitemap(), robotsTxt()],
+  integrations: [tailwind(), vue(), sitemap(), robotsTxt(), partytown()],
   vite: {
     ssr: {
       external: ["svgo"],
