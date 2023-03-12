dfec6e6f939d2319e6436ccd04cadba07a4e39e4
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-03-12 11:06:26 -0300

    feat: animate icons

---
+++ src/components/animations/FloatingIcons.vue
@@ -0,0 +1,61 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="168"
+    height="160"
+    fill="none"
+    id="vue-icon"
+  >
+    <g filter="url(#a)">
+      <path
+        fill="#41B883"
+        d="M100.661 56h11.165l-27.913 48L56 56h21.355l6.558 11.13L90.333 56h10.328Z"
+      />
+      <path
+        fill="#41B883"
+        d="m56 56 27.913 48 27.913-48h-11.165L83.913 84.8 67.027 56H56Z"
+      />
+      <path
+        fill="#35495E"
+        d="m67.027 56 16.886 28.939L100.661 56H90.333l-6.42 11.13L77.355 56H67.027Z"
+      />
+    </g>
+    <defs>
+      <filter
+        id="a"
+        width="167.826"
+        height="160"
+        x="0"
+        y="0"
+        color-interpolation-filters="sRGB"
+        filterUnits="userSpaceOnUse"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          result="hardAlpha"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        />
+        <feOffset />
+        <feGaussianBlur stdDeviation="28" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix
+          values="0 0 0 0 0.254902 0 0 0 0 0.721569 0 0 0 0 0.513726 0 0 0 1 0"
+        />
+        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_51_70" />
+        <feBlend
+          in="SourceGraphic"
+          in2="effect1_dropShadow_51_70"
+          result="shape"
+        />
+        <feGaussianBlur
+          result="effect2_foregroundBlur_51_70"
+          stdDeviation=".5"
+        />
+      </filter>
+    </defs>
+  </svg>
+</template>
+
+<script setup lang="ts">
+</script>
