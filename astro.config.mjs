418c446ebe8e024d8d3a13f298ce3ebac928f8f6
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-04-20 14:23:10 -0300

    fix: site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
-import { defineConfig } from 'astro/config';
-import partytown from '@astrojs/partytown';
-import tailwind from "@astrojs/tailwind";
-import vue from "@astrojs/vue";
-import sitemap from "@astrojs/sitemap";
-import robotsTxt from 'astro-robots-txt';
-import image from "@astrojs/image";
+import { defineConfig } from 'astro/config'
+import partytown from '@astrojs/partytown'
+import tailwind from '@astrojs/tailwind'
+import vue from '@astrojs/vue'
+import sitemap from '@astrojs/sitemap'
+import robotsTxt from 'astro-robots-txt'
+// import image from "@astrojs/image";
+
+const { CF_PAGES_URL } = process.env || {}
 
 // https://astro.build/config
 export default defineConfig({
+  site: CF_PAGES_URL || 'https://lucasribeiro.dev.br',
   integrations: [tailwind(), vue(), sitemap(), robotsTxt(), partytown()],
   vite: {
     ssr: {
-      external: ["svgo"],
+      external: ['svgo'],
     },
   },
-});
+})
