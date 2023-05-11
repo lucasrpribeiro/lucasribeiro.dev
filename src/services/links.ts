38ac5abf14d7a71056d2d036340c4099a5e1a5e6
Lucas <lucas.ribeiro711@gmail.com>
2023-05-11 12:20:16 -0300

    fix: footer vue to astro

---
+++ src/services/links.ts
@@ -0,0 +1,17 @@
+export const socialLinks = [
+  {
+    href: 'https://instagram.com/',
+    icon: 'images/icones/footer/instagram.svg',
+    alt: 'instagram',
+  },
+  {
+    href: 'https://www.linkedin.com/in/lucasribeirolr/',
+    icon: 'images/icones/footer/linkedin.svg',
+    alt: 'linkedin',
+  },
+  {
+    href: 'https://github.com/lukinhas711',
+    icon: 'images/icones/footer/github.svg',
+    alt: 'github',
+  },
+];
